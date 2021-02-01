584f6602898814930b64f6bfd7408ebdd04c73ea
Sam Hall <mann.samantha11@gmail.com>
2021-02-01 17:42:49 -0500

    commit tree 499bc371dccd8f880ab8389cd4959fcdb8a96409

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
-// Write your solution here!
+const drivers = ["Milo", "Otis", "Garfield"];
+
+// drivers.push("Milo", "Otis", "Garfield")
+
+function destructivelyAppendDriver(name) {
+    drivers.push("Ralph")
+}
+
+function destructivelyPrependDriver(name) {
+    drivers.unshift("Bob")
+}
+
+function destructivelyRemoveLastDriver(name) {
+    drivers.pop("Garfield")
+}
+
+function destructivelyRemoveFirstDriver(name) {
+    drivers.shift("Milo")
+}
+
+const copyOfDrivers = drivers.slice()
+
+function appendDriver(name) {
+    copyOfDrivers.push("Broom")
+    return copyOfDrivers
+}
+
+const secondCopyOfDrivers = drivers.slice()
+
+function prependDriver(name) {
+    secondCopyOfDrivers.unshift("Arnold")
+    return secondCopyOfDrivers
+}
+    
+const thirdCopyOfDrivers = drivers.slice()
+
+function removeLastDriver(name) {
+    thirdCopyOfDrivers.pop("Garfield")
+    return thirdCopyOfDrivers
+}
+
+const fourthCopyOfDrivers = drivers.slice()
+
+function removeFirstDriver(name) {
+    fourthCopyOfDrivers.shift("Milo")
+    return fourthCopyOfDrivers
+}
